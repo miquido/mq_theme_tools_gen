e65df3a7c9776a45aaeab28fb919b70e06ed477c
Krzysztof Hoffmann <krzysiekpl@gmail.com>
2024-02-02 15:03:07 +0100

    feat: add generated example file

---
+++ example/bin/example.g.dart
@@ -0,0 +1,139 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'example.dart';
+
+// **************************************************************************
+// ProjectExtensionsGenerator
+// **************************************************************************
+
+mixin _AppSizesMixin {
+  static const double p4 = 4;
+
+  static const double p8 = 8;
+
+  static const double p12 = 12;
+
+  static const double p16 = 16;
+
+  static const double p20 = 20;
+
+  static const double p24 = 24;
+
+  static const double p32 = 32;
+
+  static const double p48 = 48;
+
+  static const double p64 = 64;
+
+  static const double p80 = 80;
+}
+
+/// Constant horizontal gaps
+
+const hGap4 = SizedBox(width: _AppSizesMixin.p4);
+
+const hGap8 = SizedBox(width: _AppSizesMixin.p8);
+
+const hGap12 = SizedBox(width: _AppSizesMixin.p12);
+
+const hGap16 = SizedBox(width: _AppSizesMixin.p16);
+
+const hGap20 = SizedBox(width: _AppSizesMixin.p20);
+
+const hGap24 = SizedBox(width: _AppSizesMixin.p24);
+
+const hGap32 = SizedBox(width: _AppSizesMixin.p32);
+
+const hGap48 = SizedBox(width: _AppSizesMixin.p48);
+
+const hGap64 = SizedBox(width: _AppSizesMixin.p64);
+
+const hGap80 = SizedBox(width: _AppSizesMixin.p80);
+
+/// Constant vertical gaps
+
+const vGap4 = SizedBox(height: _AppSizesMixin.p4);
+
+const vGap8 = SizedBox(height: _AppSizesMixin.p8);
+
+const vGap12 = SizedBox(height: _AppSizesMixin.p12);
+
+const vGap16 = SizedBox(height: _AppSizesMixin.p16);
+
+const vGap20 = SizedBox(height: _AppSizesMixin.p20);
+
+const vGap24 = SizedBox(height: _AppSizesMixin.p24);
+
+const vGap32 = SizedBox(height: _AppSizesMixin.p32);
+
+const vGap48 = SizedBox(height: _AppSizesMixin.p48);
+
+const vGap64 = SizedBox(height: _AppSizesMixin.p64);
+
+const vGap80 = SizedBox(height: _AppSizesMixin.p80);
+
+// Vertical Paddings
+
+const vPadding4 = EdgeInsets.symmetric(vertical: _AppSizesMixin.p4);
+
+const vPadding8 = EdgeInsets.symmetric(vertical: _AppSizesMixin.p8);
+
+const vPadding12 = EdgeInsets.symmetric(vertical: _AppSizesMixin.p12);
+
+const vPadding16 = EdgeInsets.symmetric(vertical: _AppSizesMixin.p16);
+
+const vPadding20 = EdgeInsets.symmetric(vertical: _AppSizesMixin.p20);
+
+const vPadding24 = EdgeInsets.symmetric(vertical: _AppSizesMixin.p24);
+
+const vPadding32 = EdgeInsets.symmetric(vertical: _AppSizesMixin.p32);
+
+const vPadding48 = EdgeInsets.symmetric(vertical: _AppSizesMixin.p48);
+
+const vPadding64 = EdgeInsets.symmetric(vertical: _AppSizesMixin.p64);
+
+const vPadding80 = EdgeInsets.symmetric(vertical: _AppSizesMixin.p80);
+
+// Horizontal Paddings
+
+const hPadding4 = EdgeInsets.symmetric(horizontal: _AppSizesMixin.p4);
+
+const hPadding8 = EdgeInsets.symmetric(horizontal: _AppSizesMixin.p8);
+
+const hPadding12 = EdgeInsets.symmetric(horizontal: _AppSizesMixin.p12);
+
+const hPadding16 = EdgeInsets.symmetric(horizontal: _AppSizesMixin.p16);
+
+const hPadding20 = EdgeInsets.symmetric(horizontal: _AppSizesMixin.p20);
+
+const hPadding24 = EdgeInsets.symmetric(horizontal: _AppSizesMixin.p24);
+
+const hPadding32 = EdgeInsets.symmetric(horizontal: _AppSizesMixin.p32);
+
+const hPadding48 = EdgeInsets.symmetric(horizontal: _AppSizesMixin.p48);
+
+const hPadding64 = EdgeInsets.symmetric(horizontal: _AppSizesMixin.p64);
+
+const hPadding80 = EdgeInsets.symmetric(horizontal: _AppSizesMixin.p80);
+
+// All Paddings
+
+const allPadding4 = EdgeInsets.all(_AppSizesMixin.p4);
+
+const allPadding8 = EdgeInsets.all(_AppSizesMixin.p8);
+
+const allPadding12 = EdgeInsets.all(_AppSizesMixin.p12);
+
+const allPadding16 = EdgeInsets.all(_AppSizesMixin.p16);
+
+const allPadding20 = EdgeInsets.all(_AppSizesMixin.p20);
+
+const allPadding24 = EdgeInsets.all(_AppSizesMixin.p24);
+
+const allPadding32 = EdgeInsets.all(_AppSizesMixin.p32);
+
+const allPadding48 = EdgeInsets.all(_AppSizesMixin.p48);
+
+const allPadding64 = EdgeInsets.all(_AppSizesMixin.p64);
+
+const allPadding80 = EdgeInsets.all(_AppSizesMixin.p80);
